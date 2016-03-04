0cbedbe0c40e0692bb6e6940b18a816b036c5eda
DanielBerkbox <daniel.berkbox@gmail.com>
2016-03-04 15:44:23 -0300

    get coins

---
+++ JumpFuncs.js
@@ -0,0 +1,21 @@
+// JavaScript source code
+
+handlers.getPlayerData = function (args) {
+
+    var playerCoins = 0;
+    var playerCurrency = server.AddUserVirtualCurrency({
+        PlayFabId: currentPlayerId,
+        VirtualCurrency: "GO",
+        Amount : 0
+        }
+    )
+
+    if (playerCurrency.Data["Balance"]) {
+        //log.info("Player " + currentPlayerId + " already completed level " + levelNum);
+        playerCoins = playerCurrency.Data["Balance"];
+    }
+
+    return {
+        coins: playerCoins
+    };
+}
